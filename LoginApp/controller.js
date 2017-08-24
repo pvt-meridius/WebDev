5633a644c33bc55e9700170bcef9ca89696aa947
Max <pvt.meridius@gmail.com>
2017-08-23 17:41:25 -0700

    Complete

--- LoginApp/controller.js
+++ LoginApp/controller.js
@@ -6,6 +6,13 @@ app.config(function($routeProvider){
 		templateUrl: 'login.html'
 	})
 	.when('/dashboard', {
+		resolve: {
+			"check": function($location, $rootScope) {
+				if (!$rootScope.loggedIn){
+					$location.path('/');
+				}
+			}
+		},
 		templateUrl: 'dashboard.html'
 	})
 	.otherwise({
@@ -13,11 +20,10 @@ app.config(function($routeProvider){
 	});
 });
 
-app.controller('loginCtrl', function($scope, $location) {
+app.controller('loginCtrl', function($scope, $location, $rootScope) {
 	$scope.submit = function() {
-		var uname = $scope.username;
-		var password = $scope.password;
 		if($scope.username == 'admin' && $scope.password == 'admin') {
+			$rootScope.loggedIn = true;
 			$location.path('/dashboard');
 		}else{
 			alert('Wrong Authentication');
